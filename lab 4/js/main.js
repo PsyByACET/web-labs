139983407959228546acb833acfdb707ac8acca2
Alexey_Gerasimov <web-47@mail.ru>
2020-03-19 11:23:16 +0300

    add full adaprive for MainPage

--- lab 4/js/main.js
+++ lab 4/js/main.js
@@ -1,3 +1,11 @@
+if (document.documentElement.clientWidth <= 768) {
+  var sl = 1
+}
+else
+{
+  sl = 2
+}
+
 var swiper = new Swiper('.catSwiper', {
     navigation: {
       nextEl: '.swiper-button-next_catM',
@@ -5,10 +13,11 @@ var swiper = new Swiper('.catSwiper', {
     },
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
       },
   });
 var swiper2 = new Swiper('.swiper-mode-my-swiper', {
-  slidesPerView: 2,
+  slidesPerView: sl,
   spaceBetween: 30,
   navigation: {
     nextEl: '.swiper-button-next_cat2',
@@ -16,6 +25,13 @@ var swiper2 = new Swiper('.swiper-mode-my-swiper', {
   },
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
     centeredSlides: true,
   },
 });
+
+window.onload=function(){
+  var h = document.getElementById('sCont').clientHeight;
+  console.log(h);
+  document.getElementById("map").style.height = h + "px";
+}
