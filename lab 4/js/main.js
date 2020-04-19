2e699a249fe7c482f35136195d268670f86087e1
Alexey Gerasimov <web-47@mail.ru>
2020-04-19 13:36:57 +0300

    finish catalog page and add last page(page  order)

--- lab 4/js/main.js
+++ lab 4/js/main.js
@@ -30,8 +30,108 @@ var swiper2 = new Swiper('.swiper-mode-my-swiper', {
   },
 });
 
+$("a.scroll-to").on("click", function(e){
+  e.preventDefault();
+  var anchor = $(this).attr('href');
+  $('html, body').stop().animate({
+      scrollTop: $(anchor).offset().top - 60
+  }, 800);
+});
+// когда страница готова
 window.onload=function(){
-  var h = document.getElementById('sCont').clientHeight;
-  console.log(h);
+  // подгон размера карты под размер  
+  let h = document.getElementById('sCont').clientHeight;
   document.getElementById("map").style.height = h + "px";
 }
+
+
+
+var sss
+$('.check_b0x').click(function(){
+  sss = $(':checkbox:checked').length;
+	if (sss>0){
+		$('#but_filt').show(200);
+	} else {
+		$('#but_filt').hide(200);
+	}
+}); 
+
+
+// Сбросить галочки
+$('.button_reset').click(function() {
+  $('input:checked').prop('checked', false);
+  $('#but_filt').hide(200);
+});
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+
+
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+});
+
+$(document).ready(function($) {
+	$('.popup-open2').click(function() {
+		$('.popup-fade2').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close2').click(function() {
+		$(this).parents('.popup-fade2').fadeOut();
+		return false;
+  });
+  $('.popup-close-button').click(function() {
+		$(this).parents('.popup-fade2').fadeOut();
+		return false;
+	});		
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade2').fadeOut();
+		}
+	});
+	
+	$('.popup-fade2').click(function(e) {
+		if ($(e.target).closest('.popup2').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+});
